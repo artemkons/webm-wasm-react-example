b9d3c55f874df046b4029f6ba47e285ff4e4ae7d
artemkons <konstantinov050701@gmail.com>
2023-04-20 13:07:02 +0300

    Init commit

--- src/index.js
+++ src/index.js
@@ -4,6 +4,36 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const loadWebmWasm = async () => {
+  const buffer = await fetch(
+    "https://unpkg.com/webm-wasm@0.4.1/dist/webm-worker.js"
+  ).then(r => r.arrayBuffer());
+  const worker = new Worker(
+    URL.createObjectURL(new Blob([buffer], { type: "text/javascript" }))
+  );
+  worker.postMessage("https://unpkg.com/webm-wasm@0.4.1/dist/webm-wasm.wasm");
+
+  await nextMessage(worker);
+
+  worker.postMessage({
+    width: 512,
+    height: 512
+  });
+}
+
+function nextMessage(target) {
+  return new Promise(resolve => {
+    if ("once" in target) {
+      return target.once("message", resolve);
+    }
+    return target.addEventListener("message", e => resolve(e.data), {
+      once: true
+    });
+  });
+}
+
+loadWebmWasm()
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
